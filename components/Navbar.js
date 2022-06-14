c52faea92706c65ee7899fed908b23704ff7c360
Kep <KepCreations@gmail.com>
2022-06-13 21:55:56 -0400

    Initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+import CodeIcon from '@mui/icons-material/Code';
+
+import { Box, Menu, MenuItem, Stack } from '@mui/material';
+
+import resume from '../static/kep-kaeppeler-resume.pdf';
+
+const Navbar = () => {
+  const [ anchorElNav, setAnchorElNav ] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const navLinks = [
+    {
+      name: 'About',
+      href: '#about',
+      target: false
+    },
+    {
+      name: 'Products',
+      href: '#products',
+      target: false
+    },
+    {
+      name: 'Contact',
+      href: '#contact',
+      target: false
+    }
+  ];
+
+  return (
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IconButton
+            href="/"
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="code icon"
+          >
+            <CodeIcon sx={{ display: { md: 'flex' }, mr: 1 }} />
+          </IconButton>
+
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { md: 'flex' },
+              fontFamily: 'asap',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+              flexGrow: 1
+            }}
+          >
+            Kep Kaeppeler
+          </Typography>
+
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="headings of website"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' }
+              }}
+            >
+              {navLinks.map((link) => (
+                <MenuItem key={link.name} onClick={handleCloseNavMenu}>
+                  <Button
+                    color="inherit"
+                    href={link.href}
+                    target={link.target ? '_blank' : ''}
+                  >
+                    {link.name}
+                  </Button>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
+          >
+            {navLinks.map((link) => (
+              <Button
+                key={link.name}
+                color="inherit"
+                href={link.href}
+                target={link.target ? '_blank' : ''}
+              >
+                {link.name}
+              </Button>
+            ))}
+          </Stack>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Navbar;
---
+++ pages/products.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+
+const Products = () => {
+  return (
+    <main>
+      <h2>
+        <strong>Our Products</strong>
+        <p>
+          R&P Plastics, LLC offers a wide variety of product options. We
+          specialize in manufacturing narrow width tubing starting at 1" wide
+          and many sizes of poly bags up to 38" wide. R&P is committed to
+          providing our customers with the highest quality of products
+          available. We proudly use FDA approved materials with the proper
+          certifications. All of our products are proudly made in the U.S.A.<br />
+          <br />Please see a list of our available products below:<br />
+          <br />
+          <br />
+        </p>
+      </h2>
+    </main>
+  );
+};
+
+export default Products;

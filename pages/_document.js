5e02a415f60fcfe6b85823e1e21f6e32623952aa
Kep <KepCreations@gmail.com>
2022-06-25 22:44:58 -0400

    Fix favicon link error

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,6 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/static/favicon.ico" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
